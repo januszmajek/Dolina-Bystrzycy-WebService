2d00b5e2cabb9a7979cc0eb078c0606c74265c3f
Janusz Majek <januszmajekjm@gmail.com>
2021-11-07 19:32:09 +0100

    Add map layer controller
    added app layer controller, deleted polygon, added new layer from OSM,

--- app.js
+++ app.js
@@ -1,23 +1,62 @@
 //map initialization
 const map = L.map("map").setView([51.019647, 16.776968], 12);
-// add the OpenStreetMap tiles
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+// add the OpenStreetMap tiles layer
+const osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution:
     '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
-}).addTo(map);
+});
+osm.addTo(map);
+
+// add OSM hike bike layer
+const hikeBike = L.tileLayer(
+  "https://tiles.wmflabs.org/hikebike/{z}/{x}/{y}.png",
+  {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }
+);
 
 // show the scale bar on the lower left corner
 L.control.scale({ imperial: false, metric: true }).addTo(map);
 
 //adding GEOJSON
 L.geoJSON(points).addTo(map);
-L.geoJSON(lines).addTo(map);
-L.geoJSON(test).addTo(map);
-
+const parkBorder = L.geoJSON(lines);
+parkBorder.addTo(map);
 /* returns true if marker's category is "vievpoint", else returns false and writes results to console*/
-console.log(
-  markerPoints.features.map((prp) => {
-    return prp.properties.category === "viewpoint";
-  })
-);
+// console.log(
+//   points.features.map((prp) => {
+//     return prp.properties.category === "viewpoint";
+//   })
+// );
+
+// layer controller
+
+const mapLayers = {
+  "Basic OpenStreetMap": osm,
+  "Hike and Bike OpenStreetMap": hikeBike,
+};
+const catViewpoint = points.features.map((prp) => {
+  return prp.properties.category === "viewpoint";
+});
+
+const catMonument = points.features.map((prp) => {
+  return prp.properties.category === "monument";
+});
+
+const catInfo = points.features.map((prp) => {
+  return prp.properties.category === "info";
+});
+
+const markerPoints = {
+  "Punkty widokowe": catViewpoint,
+  "Tablice informacyjne": catInfo,
+};
+
+const borderLine = {
+  "Granica Parku": parkBorder,
+};
+
+L.control.layers(mapLayers, borderLine).addTo(map);
