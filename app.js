60f83eb706412fac86f23ab1d6277cc38d366843
Janusz Majek <januszmajekjm@gmail.com>
2021-11-08 17:37:52 +0100

    Add layer controller
    lines.js/geojson was renamed to parkBorder.js/geojson
    polygon.js/geojson was deleted
    points.js/geojson was splitted into new files (pointsMonument, pointsView, pointsInfo)
    app.js has layer controller now
    index.html was updated due to changes in names in other files

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 //map initialization
-const map = L.map("map").setView([51.019647, 16.776968], 12);
+const map = L.map("map", {
+  center: [51.019647, 16.776968],
+  zoom: 12,
+  maxZoom: 18,
+});
 // add the OpenStreetMap tiles layer
 const osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
   attribution:
     '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
 });
@@ -12,7 +15,6 @@ osm.addTo(map);
 const hikeBike = L.tileLayer(
   "https://tiles.wmflabs.org/hikebike/{z}/{x}/{y}.png",
   {
-    maxZoom: 19,
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }
@@ -21,10 +23,32 @@ const hikeBike = L.tileLayer(
 // show the scale bar on the lower left corner
 L.control.scale({ imperial: false, metric: true }).addTo(map);
 
-//adding GEOJSON
-L.geoJSON(points).addTo(map);
-const parkBorder = L.geoJSON(lines);
-parkBorder.addTo(map);
+//adding GEOJSON layers
+const pInfo = L.geoJSON(pointsInfo);
+const pMonument = L.geoJSON(pointsMonument);
+const pView = L.geoJSON(pointsView);
+const pBorder = L.geoJSON(parkBorder);
+pInfo.addTo(map);
+pMonument.addTo(map);
+pView.addTo(map);
+pBorder.addTo(map);
+
+// layer controller
+const mapLayers = {
+  "Basic OpenStreetMap": osm,
+  "Hike and Bike OpenStreetMap": hikeBike,
+};
+
+// object witch marker points used in layer controller
+const mapMarkers = {
+  "Granica parku": pBorder,
+  "Tablice Informacyjne": pInfo,
+  Zabytki: pMonument,
+  "Punkty widokowe": pView,
+};
+
+L.control.layers(mapLayers, mapMarkers).addTo(map);
+
 /* returns true if marker's category is "vievpoint", else returns false and writes results to console*/
 // console.log(
 //   points.features.map((prp) => {
@@ -32,31 +56,32 @@ parkBorder.addTo(map);
 //   })
 // );
 
-// layer controller
+// const catViewpoint = pointsView.features.map((prp) => {
+//   return prp.properties.category === "viewpoint";
+// });
 
-const mapLayers = {
-  "Basic OpenStreetMap": osm,
-  "Hike and Bike OpenStreetMap": hikeBike,
-};
-const catViewpoint = points.features.map((prp) => {
-  return prp.properties.category === "viewpoint";
-});
+// const catMonument = pointsMonument.features.map((prp) => {
+//   if (prp.properties.category === "monument") {
+//     return prp;
+//   } else {
+//     return false;
+//   }
+// });
 
-const catMonument = points.features.map((prp) => {
-  return prp.properties.category === "monument";
-});
+// const catInfo = pointsInfo.features.map((prp) => {
+//   return prp.properties.category === "info";
+// });
 
-const catInfo = points.features.map((prp) => {
-  return prp.properties.category === "info";
-});
+// console.log(catMonument);
+// console.log(points.features.length);
 
-const markerPoints = {
-  "Punkty widokowe": catViewpoint,
-  "Tablice informacyjne": catInfo,
-};
-
-const borderLine = {
-  "Granica Parku": parkBorder,
-};
-
-L.control.layers(mapLayers, borderLine).addTo(map);
+// const catMonument2 = points.features.map((prp) => {
+//   for (let i = 0; i <= points.features.length; i++) {
+//     if (prp.properties.category !== "monument") {
+//       continue;
+//     } else {
+//       return prp;
+//     }
+//   }
+// });
+// console.log(catMonument2);
