6b9ded05ab75384a424b11aadee48b1e097c41da
Janusz Majek <januszmajekjm@gmail.com>
2021-11-09 12:53:41 +0100

    Add custom marker icons to points
    app.js has now function that adds geojson files with customized icons
    styles folder has been added with folder markers
    markers folder includes icon's png/svg files
    data folder .js files has changed only one first letter in "icon" properities to upper letter

--- app.js
+++ app.js
@@ -23,23 +23,40 @@ const hikeBike = L.tileLayer(
 // show the scale bar on the lower left corner
 L.control.scale({ imperial: false, metric: true }).addTo(map);
 
+function markerPoint(layerData, urlIcon) {
+  const customIcon = new L.Icon({
+    iconUrl: urlIcon,
+    iconSize: [28, 46],
+    iconAnchor: [13, 40],
+    popupAnchor: [1, -24],
+  });
+  return L.geoJSON(layerData, {
+    pointToLayer: function (geoJsonPoint, latlng) {
+      return L.marker(latlng, { icon: customIcon });
+    },
+  });
+}
+
 //adding GEOJSON layers
-const pInfo = L.geoJSON(pointsInfo);
-const pMonument = L.geoJSON(pointsMonument);
-const pView = L.geoJSON(pointsView);
+const pInfo = markerPoint(pointsInfo, "style/markers/info_marker.svg");
+const pMonument = markerPoint(
+  pointsMonument,
+  "style/markers/landmark_marker.svg"
+);
+const pView = markerPoint(pointsView, "style/markers/camera_marker.svg");
 const pBorder = L.geoJSON(parkBorder);
 pInfo.addTo(map);
 pMonument.addTo(map);
 pView.addTo(map);
 pBorder.addTo(map);
 
-// layer controller
+// map background layers used in layer controller
 const mapLayers = {
   "Basic OpenStreetMap": osm,
   "Hike and Bike OpenStreetMap": hikeBike,
 };
 
-// object witch marker points used in layer controller
+// object witch marker points and lines used in layer controller
 const mapMarkers = {
   "Granica parku": pBorder,
   "Tablice Informacyjne": pInfo,
@@ -47,7 +64,11 @@ const mapMarkers = {
   "Punkty widokowe": pView,
 };
 
+// layer controller
 L.control.layers(mapLayers, mapMarkers).addTo(map);
+
+/* ----------------UNUSED CODE---------------- */
+/* --------------MAYBE USE LATER-------------- */
 
 /* returns true if marker's category is "vievpoint", else returns false and writes results to console*/
 // console.log(
@@ -85,3 +106,22 @@ L.control.layers(mapLayers, mapMarkers).addTo(map);
 //   }
 // });
 // console.log(catMonument2);
+
+// // replace Leaflet default blue marker with a custom icon
+// function customIcon(iconpath, latlng) {
+//   let myIcon = L.icon({
+//     iconUrl: iconpath,
+//     iconSize: [25, 41], // size of the icon
+//     iconAnchor: [12, 20], // point of the icon which will correspond to marker's location
+//     //shadowAnchor: [4, 62],  //the same for the shadow
+//     popupAnchor: [0, 0], // point from which the popup should open relative to the iconAnchor
+//   });
+//   const coordInfo = latlng.features.map((prp) => {
+//     return prp.geometry.coordinates;
+//   });
+//   return L.marker(coordInfo, { icon: myIcon });
+// }
+// // create an options object that specifies which function will called on each feature
+// let myLayerOptions = {
+//   pointToLayer: customIcon("./style/markers/camera_marker.png", pointsView),
+// };
