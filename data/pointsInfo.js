f8e8ba6a400d4b8fec533137d8a5adcf0b27bc9d
Janusz Majek <januszmajekjm@gmail.com>
2021-11-09 13:07:52 +0100

    Add style and data folder
    style folder contains markers folder
    markers folder contains svg/png images for custom marker icons
    data folder contains all data used (points and lines)

---
+++ data/pointsInfo.js
@@ -0,0 +1,195 @@
+const pointsInfo = {
+  type: "FeatureCollection",
+  name: "pointsInfo",
+  crs: { type: "name", properties: { name: "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        name: "info11Malkowice",
+        Icon: "infoMarker",
+        category: "info",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.818509111598061, 51.07728546185362],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "info5Stoszyce",
+        Icon: "infoMarker",
+        category: "info",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.780928460935531, 51.075736699613103],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { name: "info35Katy", Icon: "infoMarker", category: "info" },
+      geometry: {
+        type: "Point",
+        coordinates: [16.773464780608389, 51.032672245284012],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { name: "info36Katy", Icon: "infoMarker", category: "info" },
+      geometry: {
+        type: "Point",
+        coordinates: [16.77301028031917, 51.032804869412701],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "info40Ksieze",
+        Icon: "infoMarker",
+        category: "info",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.775341428997546, 51.033341539439938],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "info32Ksieze",
+        Icon: "infoMarker",
+        category: "info",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.775430725333806, 51.033764656823777],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "info25Ksieze",
+        Icon: "infoMarker",
+        category: "info",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.776381168022581, 51.034280163255495],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "info32aKsiaze",
+        Icon: "infoMarker",
+        category: "info",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.775695333750996, 51.034187658275108],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "info24Ksieze",
+        Icon: "infoMarker",
+        category: "info",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.77721040873557, 51.033207861786416],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "info23Ksieze",
+        Icon: "infoMarker",
+        category: "info",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.777106043539725, 51.032984804716008],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { name: "info6Ksieze", Icon: "infoMarker", category: "info" },
+      geometry: {
+        type: "Point",
+        coordinates: [16.776997131429269, 51.031969777494254],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { name: "info8Ksieze", Icon: "infoMarker", category: "info" },
+      geometry: {
+        type: "Point",
+        coordinates: [16.776201333498349, 51.031657565573141],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { name: "info1Ksieze", Icon: "infoMarker", category: "info" },
+      geometry: {
+        type: "Point",
+        coordinates: [16.778207519359775, 51.03131663961954],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "info2RybyKsieze",
+        Icon: "infoMarker",
+        category: "info",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.778307468161799, 51.030315085197685],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { name: "info1Ksieze", Icon: "infoMarker", category: "info" },
+      geometry: {
+        type: "Point",
+        coordinates: [16.776478171972524, 51.030280277493929],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "infoDomanice",
+        Icon: "infoMarker",
+        category: "info",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.587544574925818, 50.944138983886795],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "info12Samotwor",
+        Icon: "infoMarker",
+        category: "info",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.829198598759337, 51.103392011813192],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { name: "info3Galow", Icon: "infoMarker", category: "info" },
+      geometry: {
+        type: "Point",
+        coordinates: [16.815606838550256, 51.110794620068617],
+      },
+    },
+  ],
+};
---
+++ data/pointsMonument.js
@@ -0,0 +1,223 @@
+const pointsMonument = {
+  type: "FeatureCollection",
+  name: "pointsMonument",
+  crs: { type: "name", properties: { name: "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        name: "palaceSosnica",
+        Icon: "monumentMarker",
+        category: "monument",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.782581993570759, 51.031757458373775],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "palaceMalkowice",
+        Icon: "monumentMarker",
+        category: "monument",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.821502750605713, 51.075693409274116],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "churchMalkowice",
+        Icon: "monumentMarker",
+        category: "monument",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.823295638903858, 51.076235188356726],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "bridgeMalkowice",
+        Icon: "monumentMarker",
+        category: "monument",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.817936955569404, 51.077239127180938],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "palaceStoszyce",
+        Icon: "monumentMarker",
+        category: "monument",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.77874264894508, 51.076210140820372],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "churchSosnica",
+        Icon: "monumentMarker",
+        category: "monument",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.782487621518094, 51.032664961890312],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "gateKrobielowice",
+        Icon: "monumentMarker",
+        category: "monument",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.798659635828258, 51.009049171686485],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "palaceKrobielowice",
+        Icon: "monumentMarker",
+        category: "monument",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.798359982603657, 51.007011432702718],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "palaceCzernczyce",
+        Icon: "monumentMarker",
+        category: "monument",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.756011634699419, 50.972177119964407],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "palaceKamionna",
+        Icon: "monumentMarker",
+        category: "monument",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.749989757397113, 50.979048409274412],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "palaceMilin",
+        Icon: "monumentMarker",
+        category: "monument",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.693191064928104, 50.976983047404552],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "fallenMonumentMillin",
+        Icon: "monumentMarker",
+        category: "monument",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.693665035653574, 50.978410760142701],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "palaceMietkow",
+        Icon: "monumentMarker",
+        category: "monument",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.648639257374612, 50.973300180382715],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "palaceDomanice",
+        Icon: "monumentMarker",
+        category: "monument",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.586118340827905, 50.943183149570402],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "palaceAlexandrow",
+        Icon: "monumentMarker",
+        category: "monument",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.829951694698803, 51.103313952322893],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "palaceGalow",
+        Icon: "monumentMarker",
+        category: "monument",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.817967019604588, 51.110136306547624],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "monumentNepomucenGolow",
+        Icon: "monumentMarker",
+        category: "monument",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.814987065815377, 51.110700737442194],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "churchGolow",
+        Icon: "monumentMarker",
+        category: "monument",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.814583492871737, 51.110744850989192],
+      },
+    },
+  ],
+};
